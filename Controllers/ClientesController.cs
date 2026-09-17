d1c971963b36e2845b2b71288bb36eb6f750b4c6
davidtav <david.mclaurel@outlook.com.br>
2026-09-17 15:59:07 -0300

    Adicionar controller e view para gerenciamento de clientes

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestorOrdemDeServico.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
